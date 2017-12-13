b72d3ebe52b08e7cc7448ed0e469b1b9c9eb35c8
Juho Vepsalainen <bebraw@gmail.com>
2017-12-13 17:00:05 +0100

    chore: Add missing workshops

--- src/workshops/index.js
+++ src/workshops/index.js
@@ -1,9 +1,12 @@
 module.exports = {
   andreyAndArtem: require("./andrey-and-artem"),
+  davidKhourshid: require("./david-khourshid"),
   gantLaborde: require("./gant-laborde"),
   juhoVepsalainen: require("./juho-vepsalainen"),
   michelWeststrate: require("./michel-weststrate"),
   patrickAndNik: require("./patrick-and-nik"),
+  rotemMizrachiMeidan: require("./rotem-mizrachi-meidan"),
   saraVieira: require("./sara-vieira"),
+  siaKaramalegos: require("./sia-karamalegos"),
   svenSauleau: require("./sven-sauleau"),
 };
