9897ba1443125347f55f923e344de473ff2f78c5
Juho Vepsalainen <bebraw@gmail.com>
2017-12-14 12:35:03 +0100

    chore: Rename schedule as schedules
    More logical this way.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ module.exports = {
   keynotes: require("./keynotes"),
   lightningTalks: require("./lightning-talks"),
   presentations: require("./presentations"),
-  schedule: require("./schedule"),
+  schedule: require("./schedules"), // XXX: deprecated, drop
+  schedules: require("./schedules"),
   speakers: require("./speakers"),
   workshops: require("./workshops"),
 };
