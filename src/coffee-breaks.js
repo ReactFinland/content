511643ba3924b851ad2197dadba7ca08c017e91d
Juho Vepsalainen <bebraw@gmail.com>
2017-12-14 17:00:17 +0100

    chore: Push coffee breaks to an array format as well

--- src/coffee-breaks.js
+++ src/coffee-breaks.js
@@ -1,12 +1,16 @@
 module.exports = {
-  "25-04-2018": {
-    title: "Coffee break",
-    description:
-      "We don't mind if you drink tea, though. Water is available as well.",
-  },
-  "26-04-2018": {
-    title: "Coffee break",
-    description:
-      "Other beverages are available too although black coffee is our favorite.",
-  },
+  "25-04-2018": [
+    {
+      title: "Coffee break",
+      description:
+        "We don't mind if you drink tea, though. Water is available as well.",
+    },
+  ],
+  "26-04-2018": [
+    {
+      title: "Coffee break",
+      description:
+        "Other beverages are available too although black coffee is our favorite.",
+    },
+  ],
 };
