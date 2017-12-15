25aa4271a710c0d7d392319325917e5c139f7f01
Juho Vepsalainen <bebraw@gmail.com>
2017-12-15 12:23:13 +0100

    chore: Simplify lunch descriptions

--- src/lunches.js
+++ src/lunches.js
@@ -2,14 +2,13 @@ module.exports = {
   "25-04-2018": [
     {
       title: "Lunch",
-      description:
-        "Lunch. It's functional. You are **not** supposed to like it.",
+      description: "It's functional. You are **not** supposed to like it.",
     },
   ],
   "26-04-2018": [
     {
       title: "Lunch",
-      description: "Lunch. You might like it better this time.",
+      description: "You might like it better this time.",
     },
   ],
 };
