dda770db64b22c67f33854fec426fb50725b5390
Juho Vepsalainen <bebraw@gmail.com>
2018-01-12 21:06:47 +0100

    chore: Add SurviveJS as a partner

--- src/partners/index.js
+++ src/partners/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   agentConf: require("./agent-conf"),
   reactDayBerlin: require("./react-day-berlin"),
+  survivejs: require("./survivejs"),
   webexpo: require("./webexpo"),
 };
---
+++ src/partners/survivejs.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "SurviveJS",
+  about: "SurviveJS will take you from a JavaScript apprentice to master",
+  image: "survivejs.png",
+  social: {
+    homepage: "https://survivejs.com/",
+    twitter: "survivejs",
+  },
+  location: {
+    country: {
+      name: "Austria",
+      code: "AT",
+    },
+    city: "Vienna",
+  },
+};
