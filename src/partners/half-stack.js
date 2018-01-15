3f48f19b361eb55662feadb40341df3100e6d928
Juho Vepsalainen <bebraw@gmail.com>
2018-01-15 17:47:54 +0100

    chore: Add Halfstack as a partner

---
+++ src/partners/half-stack.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "Halfstack 2018",
+  about:
+    "HalfStack is a one-day, single track, fun JS conf in a Shoreditch pub",
+  image: "halfstack.svg",
+  social: {
+    homepage: "http://halfstackconf.com/",
+    facebook: "halfstackconf",
+    instagram: "halfstackconf",
+    twitter: "london_ajax",
+  },
+  location: {
+    country: {
+      name: "Great Britain",
+      code: "GB",
+    },
+    city: "London",
+  },
+};
--- src/partners/index.js
+++ src/partners/index.js
@@ -1,5 +1,6 @@
 module.exports = [
   require("./agent-conf"),
+  require("./half-stack"),
   require("./react-day-berlin"),
   require("./survivejs"),
   require("./webexpo"),
