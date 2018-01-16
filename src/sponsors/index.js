1e38abca6a655e962c54751ce0b96b4840f94775
Juho Vepsalainen <bebraw@gmail.com>
2018-01-16 18:18:22 +0100

    chore: Add ReactFest

--- src/sponsors/index.js
+++ src/sponsors/index.js
@@ -2,6 +2,7 @@ module.exports = [
   require("./agent-conf"),
   require("./half-stack"),
   require("./react-day-berlin"),
+  require("./react-fest"),
   require("./survivejs"),
   require("./webexpo"),
 ];
