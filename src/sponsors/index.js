87a87ab0b677700a5323d367ba30674c9e724958
Juho Vepsalainen <bebraw@gmail.com>
2018-01-22 22:31:17 +0100

    chore: Add React Alicante

--- src/sponsors/index.js
+++ src/sponsors/index.js
@@ -1,6 +1,7 @@
 module.exports = [
   require("./agent-conf"),
   require("./half-stack"),
+  require("./react-alicante"),
   require("./react-day-berlin"),
   require("./react-fest"),
   require("./reason-conf"),
