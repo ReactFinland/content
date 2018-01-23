5f52af431238ab780470fcf1fec138cad1f0ceb9
Joni Nevalainen <joninevalainen@gmail.com>
2018-01-23 20:20:14 +0200

    Add Joni's details to organizers

---
+++ src/organizers/joni-nevalainen.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "Joni Nevalainen",
+  about: "Joni Nevalainen works on the app.",
+  image: "joni.jpg",
+  social: {
+    homepage: "",
+    twitter: "joninevalainen",
+    github: "joni-",
+    linkedin: "nevalainenjoni",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+  },
+  keywords: [],
+};
