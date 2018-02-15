27573679a669dba47c3d56af7b9267f523eeb5b1
Juho Vepsalainen <bebraw@gmail.com>
2018-02-15 11:20:17 +0100

    fix: Remove extra space from text descriptions

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ module.exports = {
   title: "About",
   description: "What React Finland is about?",
   intro: `
-  Our goal is to connect our valued speakers with our local community in the most fruitful way we can and provide Finnish hospitality to our conference visitors.
+Our goal is to connect our valued speakers with our local community in the most fruitful way we can and provide Finnish hospitality to our conference visitors.
 
-  We hope to create something unique by combining our northern traditions with speakers and visitors from all around the world.
+We hope to create something unique by combining our northern traditions with speakers and visitors from all around the world.
 `,
 };
--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -3,10 +3,10 @@ module.exports = {
   title: "Imprint",
   description: "How to reach React Finland organizers?",
   main: `
-  React Finland ry
-  Lehdokkipolku 2 A 2
-  00930 Helsinki
+React Finland ry
+Lehdokkipolku 2 A 2
+00930 Helsinki
 
-  info <at> react-finland.fi
+info <at> react-finland.fi
 `,
 };
