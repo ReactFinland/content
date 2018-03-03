a22edd2d3d97772f431c47efd9b7b0df298fdcb9
Juho Vepsalainen <bebraw@gmail.com>
2018-03-03 08:46:34 +0100

    fix(sara): Improve description newlines

--- src/workshops/sara-vieira.js
+++ src/workshops/sara-vieira.js
@@ -5,11 +5,11 @@ module.exports = {
   title: "Testing React",
   description: `In this workshop I will be going through unit testing in Javascript, how you can make it fun and meaningful for your app.
 
-  We will start by learning all the things about unit tests and how to implement very simple tests for your functions.
+We will start by learning all the things about unit tests and how to implement very simple tests for your functions.
 
-  We will then move to React/React Native territory and learn about snapshot testing and components testing to keep regressions from happening.
+We will then move to React/React Native territory and learn about snapshot testing and components testing to keep regressions from happening.
 
-  By the end, we will implement all of this in continuous development with GitHub and Travis so you can leave with the exact idea on how to go about implementing extensive tests in your app without shedding a single tear.`,
+By the end, we will implement all of this in continuous development with GitHub and Travis so you can leave with the exact idea on how to go about implementing extensive tests in your app without shedding a single tear.`,
   tickets: [
     {
       ...require("../tickets/morning-workshop")[0],
