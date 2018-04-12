6a4ad8125f724383731db5bc49ed85a5c886b68f
Sven SAULEAU <contact@xtuc.fr>
2018-04-12 21:58:06 +0200

    fix(sven): Fix Sven's lightning talk description

--- src/talks/sven-sauleau.js
+++ src/talks/sven-sauleau.js
@@ -4,8 +4,8 @@ const speaker = require("../speakers/sven-sauleau");
 
 module.exports = {
   speakers: [speaker],
-  title: "To be announced",
-  description: "To be announced",
+  title: "Understanding the differences is accepting",
+  description: "I want to show you that the unexpected JavaScript things you encountered are understandable by humans and formally specified.",
   type: enums.LIGHTNING_TALK,
   keywords: speaker.keywords,
 };
