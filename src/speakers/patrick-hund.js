e6bda8f4f43fabc76d50cf23f28db6e6e0b0a64c
Patrick Hund <pahund@ebay.com>
2018-04-15 16:41:20 +0200

    added linked in, youtube and homepage to Patrick Hund's speaker profile

--- src/speakers/patrick-hund.js
+++ src/speakers/patrick-hund.js
@@ -6,14 +6,14 @@ module.exports = {
     "Patrick helps React and Node.js conquer the tech stack at eBay in Berlin. He has been a professional software developer since 2000 and joined eBay in 2010. Notable projects are the relaunch of the homepage of eBay's car trading platform mobile.de in 2015 and the rewrite of eBay’s automotive online community MOTOR-TALK.de (ongoing, since 2017).",
   image: "speakers/patrick-hund.jpg",
   social: {
-    homepage: "",
+    homepage: "https://ebaytech.berlin",
     twitter: "wiekatz",
     github: "pahund",
     facebook: "",
     medium: "@wiekatz",
     instagram: "",
-    linkedin: "",
-    youtube: "",
+    linkedin: "https://www.linkedin.com/in/patrickhund/",
+    youtube: "https://www.youtube.com/channel/UCF0c1u0VvFw2MU_fjqtmMbQ",
   },
   location: {
     country: {
