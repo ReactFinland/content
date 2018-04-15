74de8510e4dd61741b7b31638ebd259260b316e2
Patrick Hund <pahund@ebay.com>
2018-04-15 16:38:05 +0200

    Updated title and description of Patrick Hund's lightning talk

--- src/talks/patrick-hund.js
+++ src/talks/patrick-hund.js
@@ -4,15 +4,11 @@ const speaker = require("../speakers/patrick-hund");
 
 module.exports = {
   speakers: [speaker],
-  title: "Breaking Down Your Web App",
+  title: "Get Rich Quick With React Context",
   description: `
-Let’s face it – the question is not *if* your codebase will become a horrible, unmaintainable mess, it’s only a question of *when*…
-
-What architecture patterns and strategies are there to break down your web application’s code into cleanly separated components? How can you compose your software of “Lego bricks” that you can simply replace with shiny new ones when they have become rotten?
-
-Patrick will show you how to be proud of writing code that’s easy to throw away instead of being proud of writing code that haunts your colleagues and your future self forever!
-
-He will show some hands-on examples from his current work on the relaunch of eBay’s automotive online community MOTOR-TALK.de. While these are based on JavaScript, React and Redux, the underlying patterns will be useful for users of other languages and frameworks.
+With React 16.3, the context API has been completely revamped. This talk will demonstrate a good use 
+case for context: Putting ad placements on your web page to *get rich quick!* You'll learn how easy 
+it is to use context now and how to migrate your old context code to the new API.
   `,
   type: enums.LIGHTNING_TALK,
   keywords: speaker.keywords,
