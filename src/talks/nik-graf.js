29e1bbb66508935d4afe04ec9334fa8abe043757
Juho Vepsalainen <bebraw@gmail.com>
2018-04-19 13:25:40 +0200

    feat(talk): Update keywords of Nik's talks

--- src/talks/nik-graf.js
+++ src/talks/nik-graf.js
@@ -6,5 +6,5 @@ module.exports = {
   title: "Get started with Reason",
   description: "We will kick off with the basics and then quickly go into how to leverage features like variant types and pattern matching to make impossible states impossible. After you gained some knowledge about the basics the course will dig even further into ReasonReact.",
   type: enums.PRESENTATION,
-  keywords: require("../speakers/nik-graf").keywords,
+  keywords: [keywords.REASON, keywords.REACT],
 };
