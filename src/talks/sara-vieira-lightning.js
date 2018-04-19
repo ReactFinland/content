9f74244c0b5dd44db88f9d18183243e5076d0770
Juho Vepsalainen <bebraw@gmail.com>
2018-04-19 11:23:57 +0200

    feat(talk): Update Sara's lightning talk

--- src/talks/sara-vieira-lightning.js
+++ src/talks/sara-vieira-lightning.js
@@ -4,8 +4,10 @@ const speaker = require("../speakers/sara-vieira");
 
 module.exports = {
   speakers: [speaker],
-  title: "GraphQL and Apollo Awesomeness",
-  description: "Live coding with GraphQL and Apollo!",
+  title: "Speed dating GraphQL!",
+  description: `Ever wanted to create something with GraphQL but you think it's hard and takes too long? In this talk I will create a simple GraphQL service in Graphcool and Apollo for you too see how awesome it is!
+
+Warning : live coding! Fuck ups may happen.`,
   type: enums.LIGHTNING_TALK,
   keywords: [keywords.APOLLO, keywords.GRAPHQL],
 };
