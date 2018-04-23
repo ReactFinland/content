62a0128827915ff048779c5e278d6c018eb62fc4
Juho Vepsalainen <bebraw@gmail.com>
2018-04-23 15:00:52 +0300

    feat(talk): Update talk keywords

--- src/talks/sara-vieira.js
+++ src/talks/sara-vieira.js
@@ -6,5 +6,5 @@ module.exports = {
   title: "State Management in React Apps with Apollo Client",
   description: "Apollo has given us freedom and happiness when it comes to managing our data coming from the server but we still had to write code and sometimes a lot of it to manage our local state? Well, what if we managed it with queries too? Sounds too awesome right? Let's learn how to do this with apollo-link-state.",
   type: enums.PRESENTATION,
-  keywords: require("../speakers/sara-vieira").keywords,
+  keywords: [keywords.APOLLO, keywords.GRAPHQL],
 };
