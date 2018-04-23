fad92653733575478a3c4ee072e6c8b00c630684
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-04-23 21:47:17 +0300

    Update patrick-stapfer.js

--- src/talks/patrick-stapfer.js
+++ src/talks/patrick-stapfer.js
@@ -3,8 +3,8 @@ const keywords = require("../keywords");
 
 module.exports = {
   speakers: [require("../speakers/patrick-stapfer")],
-  title: "ReasonML",
-  description: "",
+  title: "Making Unreasonable States Impossible",
+  description: "Based on @nikgraf's introduction to Reason (Get started with Reason), this talk goes deeper into the world of variant types and pattern matching and puts them into a practical context. You will learn how these tools help you design solid APIs, which are impossible to misuse by consumers. Additionally you will get more insights into practical ReasonReact code.",
   type: enums.PRESENTATION,
   keywords: require("../speakers/patrick-stapfer").keywords,
 };
