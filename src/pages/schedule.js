efde51b49d520cf705ed3d7786a71a71fcf2a924
Juho Vepsalainen <bebraw@gmail.com>
2018-04-26 08:26:52 +0300

    feat(schedule): Make sure 26th is the first day

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -8,6 +8,22 @@ The schedule of the event has been carefully curated. In true Finnish style, we 
 > [Subscribe to the conference calendar](https://api.react-finland.fi/calendar-2018.ics). You can import it to your Google Calendar for example.
 `,
   main: `
+## 26.04—Thursday—More React
+
+The second day of the event continues where the first one left off:
+
+{schedule:@react-finland/content-2018/src/schedules/26-04-2018}
+
+## 25.04—Wednesday—React
+
+In Finnish style, we start each day early. We also eat lunch early. This way we'll have time to visit sauna, the holiest place of our culture.
+
+The first presentation day has a strict focus on React itself. Each session ends with a brief break and we have a couple longer breaks in between:
+
+{schedule:@react-finland/content-2018/src/schedules/25-04-2018}
+
+There will be fun after the panel.
+
 ## 24.04—Tuesday—[Workshops](/workshops/)
 
 Each session will take four hours and they are arranged twice so you have a chance to choose two. A session is capped to twenty people. The first sessions are from 9 to 13 and the second sessions are from 14 to 18.
@@ -28,21 +44,5 @@ The following four hour workshops are arranged only once:
 We also have a full day workshop (eight hours including a lunch):
 
 * **State Management** with Michel Weststrate, the author of MobX and Mobx-State-Tree. This session is longer, from 09 to 18. (Valkoinen Sali)
-
-## 25.04—Wednesday—React
-
-In Finnish style, we start each day early. We also eat lunch early. This way we'll have time to visit sauna, the holiest place of our culture.
-
-The first presentation day has a strict focus on React itself. Each session ends with a brief break and we have a couple longer breaks in between:
-
-{schedule:@react-finland/content-2018/src/schedules/25-04-2018}
-
-There will be fun after the panel.
-
-## 26.04—Thursday—More React
-
-The second day of the event continues where the first one left off:
-
-{schedule:@react-finland/content-2018/src/schedules/26-04-2018}
 `,
 };
